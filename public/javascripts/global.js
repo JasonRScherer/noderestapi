6736ed8d06f1108bc0561a009c70ea2dc1f2b234
Jason Scherer <Jason.Scherer@dedicatedcomputing.com>
2014-10-01 15:45:47 -0500

    Working on pagination for the search results.  Got all searching working again and organized better

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -66,25 +66,35 @@ function addMessage(event){
 };
 function searchMessage(event){
     event.preventDefault();
+    var thisResult = '';
     //Basic Validation
     var errorCount = 0;
-    $('#Results').empty();
+    $('#resultsTable tr').not(':first').remove();
+    $('#numOfResults p').remove();
     $('#searchMessage input').each(function(index, val){
         if($(this).val() === ''){ errorCount++;}
     });
     if(errorCount === 0){
-        var newMessage =  $('#searchMessage fieldset input#searchMessage').val()
+        var newMessage =  $('#searchMessage input#searchMessage').val()
         var resultUrl = 'http://172.16.7.237:8080/solr/collection1/query?q=message_s:*'+newMessage+'*&fl=message_s, date_s&wt=json&json.wrf=?&rows=1000';
         $.getJSON(resultUrl, function(result){
             var Parent = document.getElementById('resultsTable');
-            var thisResult = 'Results found:' + result.response.numFound + '<br/>';
-            alert(result.response.docs.length);
+            //var thisResult = 'Results found:' + result.response.numFound + '<br/>';
             for (var i=0; i < result.response.docs.length; i++){
-                thisResult += '<tr><td>'+result.response.docs[i].message_s + '</td><td>' +result.response.docs[i].date_s+ '</td></tr><br/>';
-                var NewDiv = document.createElement("DIV");
-                NewDiv.innerHTML = thisResult;
+                if (result.response.docs.length > 5){
+                }
+                thisResult += '<tr>';
+                thisResult += '<td>' + result.response.docs[i].message_s + '</td>';
+                thisResult += '<td>' + result.response.docs[i].date_s + '</td>';
+                thisResult += '</tr>';
             }
-            Parent.appendChild(NewDiv);
+           /* thisResult += '<tr>';
+            thisResult += '<td>Results Found:</td>';
+            thisResult += '<td align=right><b>' + result.response.numFound +'</b></td>';
+            thisResult += '</tr>';*/
+            $('#resultsTable tbody:last').append(thisResult);
+          //  paginationTable();
+            $('#numOfResults').append('<p>Results found: '+result.response.numFound.toString()+'</p>');
         });
 
     }
@@ -93,3 +103,28 @@ function searchMessage(event){
     }
 };
 
+
+function paginationTable(){
+    var maxRows = 10;
+    $('#resultsTable').each(function(){
+       /* var cTable = $(this);
+        var cRows = cTable.find('tr:gt(0)');
+        var cRowCount cRows.size();
+
+        if(cRowCount < maxRows){
+            return;
+        };
+        cRows.each(function(i){
+            $(this).find('td:first').text(fcuntion(j, val){
+                return (i + 1) + " - " + val;
+            });
+        });
+        cRows.filter(':gt(' + (maxRows - 1) +')').hide();
+
+        var cPrev = cTable.siblings('.prev');
+        var cNext = cTable.siblings('.next');
+
+        cPrev.addClass('disabled');*/
+        });
+
+};
